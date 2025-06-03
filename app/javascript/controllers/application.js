9308d30d459c45c074c43bec2d5c855edad738f3
Ibrahim Ali <ibbyali2@gmail.com>
2025-06-03 18:10:02 +0100

    pharmacist section

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import BasketController from "./controllers/basket_controller";
 
 const application = Application.start();
-application.register("basket", BasketController);
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
