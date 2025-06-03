f2ffff9fc08f72d30bb729c9715933b99ac35c26
Simson Hannawell <simsonhannawell@gmail.com>
2025-06-03 10:06:23 +0100

    simson

--- app/controllers/basket_items_controller.rb
+++ app/controllers/basket_items_controller.rb
@@ -1,57 +1,87 @@
 class BasketItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_pharmacy
+  before_action :set_basket_item, only: [:update, :destroy]
 
   def create
-    order = current_user.orders.find_or_create_by!(pharmacy: @pharmacy, delivered?: false)
-    basket = order.basket || order.create_basket(pharmacy: @pharmacy)
+  #   order = current_user.orders.find_or_create_by!(pharmacy: @pharmacy, delivered?: false)
+  #   basket = order.basket || order.create_basket(pharmacy: @pharmacy)
 
-    basket_item = basket.basket_items.find_or_initialize_by(product_id: params[:product_id])
-    basket_item.quantity += params[:quantity].to_i
-    basket_item.save!
+  #   basket_item = basket.basket_items.find_or_initialize_by(product_id: params[:product_id])
+  #   basket_item.quantity += params[:quantity].to_i
+  #   basket_item.save!
 
-    # Recalculate total using pharmacy_products pricing
-    total_price = basket.basket_items.joins("INNER JOIN pharmacy_products ON pharmacy_products.product_id = basket_items.product_id AND pharmacy_products.pharmacy_id = #{basket.pharmacy_id}")
-                                     .sum("pharmacy_products.price * basket_items.quantity")
+  #   # Recalculate total using pharmacy_products pricing
+  #   total_price = basket.basket_items.joins("INNER JOIN pharmacy_products ON pharmacy_products.product_id = basket_items.product_id AND pharmacy_products.pharmacy_id = #{basket.pharmacy_id}")
+  #                                    .sum("pharmacy_products.price * basket_items.quantity")
 
-    render json: {
-      success: true,
-      total_price: total_price,
-      basket_id: basket.id
-    }
-  rescue => e
-    Rails.logger.error("BasketItems#create error: #{e.message}")
-    render json: { error: e.message }, status: :unprocessable_entity
-  end
+  #   render json: {
+  #     success: true,
+  #     total_price: total_price,
+  #     basket_id: basket.id
+  #   }
+  # rescue => e
+  #   Rails.logger.error("BasketItems#create error: #{e.message}")
+  #   render json: { error: e.message }, status: :unprocessable_entity
+  @pharmacy = Pharmacy.find(params[:pharmacy_id])
 
+  @basket = @pharmacy.baskets.last || Basket.new(pharmacy: @pharmacy)
+  @basket.pharmacy = @pharmacy
+  @basket_item = @basket.basket_items.find_or_initialize_by(product_id: params[:basket_item][:product_id])
 
-  def update
-    basket_item = BasketItem.find(params[:id])
-    if basket_item.update(basket_item_params)
-      redirect_to pharmacy_path(basket_item.basket.pharmacy), notice: 'Basket item updated successfully.'
+  @basket_item.quantity = params[:basket_item][:quantity]
+
+    @total_price = @basket.basket_items.sum do |item|
+      product_price(item)
+    end
+    if @basket_item.save
+      redirect_to basket_path(@basket), notice: "Item added to basket."
     else
-      redirect_to pharmacy_path(basket_item.basket.pharmacy), alert: 'Failed to update basket item.'
+      redirect_to pharmacy_path(@pharmacy), alert: "Failed to add item to basket."
     end
   end
 
+ def update
+  @basket_item = BasketItem.find(params[:id])
+  if @basket_item.update(basket_item_params)
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace("basket-item-#{@basket_item.id}", partial: "basket_items/item", locals: { item: @basket_item }) }
+      format.html { redirect_to basket_path, notice: "Quantity updated." }
+    end
+  else
+    # handle error
+  end
+end
+
   def destroy
-    basket_item = BasketItem.find(params[:id])
-    pharmacy = basket_item.basket.pharmacy
-    if basket_item.destroy
-      redirect_to pharmacy_path(pharmacy), notice: 'Item removed from basket.'
-    else
-      redirect_to pharmacy_path(pharmacy), alert: 'Failed to remove item from basket.'
+    @basket = @basket_item.basket
+    @basket_item.destroy
+    @total_price = @basket.basket_items.sum do |item|
+      product_price(item)
+    end
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to basket_path(@basket), notice: "Item removed" }
     end
   end
 
   private
 
-  def basket_item_params
-    params.require(:basket_item).permit(:product_id, :quantity)
-  end
-
   def set_pharmacy
     @pharmacy = Pharmacy.find(params[:pharmacy_id])
   end
 
+  def set_basket_item
+    @basket_item = BasketItem.find(params[:id])
+  end
+
+  def basket_item_params
+    params.require(:basket_item).permit(:quantity)
+  end
+
+  def product_price(item)
+    @pharmacy_product = PharmacyProduct.find_by(pharmacy: @pharmacy, product: item.product)
+    @pharmacy_product.price * item.quantity
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_29_143937) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_28_115312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,22 +68,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_29_143937) do
     t.index ["user_id"], name: "index_favourites_on_user_id"
   end
 
-  create_table "medications", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.decimal "price"
-    t.text "images"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.boolean "delivered?", default: false
     t.bigint "basket_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.index ["basket_id"], name: "index_orders_on_basket_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
@@ -124,7 +114,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_29_143937) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal "price"
   end
 
   create_table "reviews", force: :cascade do |t|
