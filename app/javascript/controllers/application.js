4457b42536d1b9d66ba74e712f043bdcc7ecfd38
Namra Mahmood <namramahmood423@gmail.com>
2025-06-04 15:02:00 +0100

    updated

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import BasketController from "./controllers/basket_controller";
 
 const application = Application.start();
-<<<<<<< HEAD
-application.register("basket", BasketController);
-=======
->>>>>>> 0a8bf63839e9fd9aba9c42ca38c8a593f039522f
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
