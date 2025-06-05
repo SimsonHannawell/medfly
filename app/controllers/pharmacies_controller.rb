dd08582f9739b79c70b5f55a20189bdf545509da
Namra Mahmood <namramahmood423@gmail.com>
2025-06-05 17:55:55 +0100

    basket page updated

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -82,10 +82,6 @@ class PharmaciesController < ApplicationController
 
   private
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
   def pharmacy_params
     params.require(:pharmacy).permit(:name, :address, :phone_number, :email)
   end
