18bee9279aaed8493fb4473f61af2f8cdaf41d44
Ibrahim Ali <ibbyali2@gmail.com>
2025-06-05 17:12:22 +0100

    maps

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -31,3 +31,11 @@
   #units: :km, # Defaults to miles (:mi)
   ## [...]
 #)
+Geocoder.configure(
+  # [...]
+  timeout: 5, # seconds
+  lookup: :mapbox, # Defaults to :nominatim, which is less reliable but free
+  api_key: ENV['MAPBOX_API_KEY'],
+  units: :km, # Defaults to miles (:mi)
+  # [...]
+)
